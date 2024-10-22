aaf04a4d415190881d6afd20f0ceba7ead8bffaf
rubinnn <e1155263@u.nus.edu>
2024-10-22 13:12:05 +0800

    Update Predicate
    Update Predicate Code

--- src/main/java/seedu/address/model/person/DescriptionContainsKeywordPredicate.java
+++ src/main/java/seedu/address/model/person/DescriptionContainsKeywordPredicate.java
@@ -1,4 +1,23 @@
 package seedu.address.model.person;
 
-public class DescriptionContainsKeywordPredicate {
+import java.util.function.Predicate;
+
+public class DescriptionContainsKeywordPredicate implements Predicate<Person> {
+    private final String keyword;
+
+    public DescriptionContainsKeywordPredicate(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return person.getDescription().value.toLowerCase().contains(keyword.toLowerCase());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DescriptionContainsKeywordPredicate // instanceof handles nulls
+                && keyword.equals(((DescriptionContainsKeywordPredicate) other).keyword)); // state check
+    }
 }
